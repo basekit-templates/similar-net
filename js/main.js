f8b84a2f07b61af8dd9be8d41ba85b2b16bd8c10
rhysjparry <rhys.parry@basekit.com>
2015-12-22 10:06:29 +0000

    Initial commit.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,25 @@
 $( ".navigation-toggle").click(function() {
     $( ".navigation-toggle, .navigation" ).toggleClass( "active" );
 });
+
+
+// Pricing Table Toggle
+$( ".one-pager .price-plan").click(function() {
+    $( ".one-pager" ).toggleClass( "open" );
+});
+
+$( ".basic .price-plan").click(function() {
+    $( ".basic" ).toggleClass( "open" );
+});
+
+$( ".business .price-plan").click(function() {
+    $( ".business" ).toggleClass( "open" );
+});
+
+$( ".mega .price-plan").click(function() {
+    $( ".mega" ).toggleClass( "open" );
+});
+
+$( ".button-video, .close-icon").click(function() {
+    $( ".editor-video" ).toggleClass( "play" );
+});
